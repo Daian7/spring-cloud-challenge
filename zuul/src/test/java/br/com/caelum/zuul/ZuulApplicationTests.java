542bcea7e59167212142846bcdc4c39981366512
Daian Mauro de Almeida <daian.mauro@outlook.com>
2022-03-23 01:01:40 -0300

    API Gateway com zuul

---
+++ zuul/src/test/java/br/com/caelum/zuul/ZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caelum.zuul;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ZuulApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
